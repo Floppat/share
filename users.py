ffdcbc07408d615c2bc7700555a8d2153d802f61
Floppat <164739638+Floppat@users.noreply.github.com>
2024-04-25 13:24:50 +0300

    Update users.py

--- users.py
+++ users.py
@@ -18,18 +18,16 @@ class User:
         if not self.pet.can_train():
             await interaction.response.send_message('сперва вашему питомцу следует восстановить силы')
             return
-        await interaction.response.send_message('питомец прошёл изнурительные тренеровки')
         self.pet.train()
-        await interaction.response.send_message(self.pet)
+        await interaction.response.send_message(f'питомец прошёл изнурительные тренеровки{self.pet}')
         return
 
     async def feed(self, interaction: Interaction ):
         if not self.pet.can_feed():
             await interaction.response.send_message('Ваш питомец не голоден')
             return
-        await interaction.response.send_message('питомец сытно поел')
         self.pet.feed()
-        await interaction.response.send_message(self.pet)
+        await interaction.response.send_message(f'питомец сытно поел{self.pet}')
         return
 
     async def attack(self, interaction: Interaction ):
@@ -37,16 +35,14 @@ class User:
             await interaction.response.send_message('сперва вашему питомцу следует восстановить силы')
             return
         enemy = Enemy(target_pet=self.pet)
-        await interaction.response.send_message('вы атакуете врага')
         self.pet.stamina -= 60
         while True:
             enemy.attack(target_pet=self.pet)
             self.pet.attack(target_pet=enemy)
-            await interaction.response.send_message(f'ваш питомец: {self.pet}\n'
-                            f'ваш враг: {enemy}')
 
             if not self.pet:
-                await interaction.response.send_message('вы проиграли!')
+                await interaction.response.send_message(f'вы проиграли!, ваш враг был:{enemy}\n'
+                                                        f'ваши характеристики:{self.pet}')
                 return
 
             if not enemy:
@@ -54,41 +50,37 @@ class User:
                 self.pet.minlvl += 2
                 self.pet.maxlvl += 2
                 self.pet.floor += 1
-                await interaction.response.send_message('вы выиграли. противник стал посильнее.\n'
+                await interaction.response.send_message(f'вы выиграли. противник стал посильнее. ваш враг был:{enemy}\n'
                                 'вы заработали 100 монет\n'
-                                f'итого монет: {self.pet.coins}')
+                                f'итого монет: {self.coins}\n'
+                                f'ваши характеристики:{self.pet}')
                 return       
     async def sleep(self, interaction: Interaction ):
         if not self.pet.can_sleep():
             await interaction.response.send_message('Ваш питомец ещё не устал')
             return
-        await interaction.response.send_message('питомец выспался')
         self.pet.sleep()
-        await interaction.response.send_message(self.pet)
+        await interaction.response.send_message(f'питомец выспался{self.pet}')
         return
-    async def shop(self, interaction: Interaction, item ):
-        await interaction.response.send_message('item=1;  400 монет')   
-        await interaction.response.send_message('item=2;  800 монет') 
-        await interaction.response.send_message('item=3;  1200 монет')
-        await interaction.response.send_message('item=4;  1600 монет')
-        await interaction.response.send_message('item=5;  2000 монет')
 
-        if item not in ('1', '2', '3', '4', '5'):
-            await interaction.response.send_message('Неправильный item, выберите из списка (1 2 3 4 5)')
+    async def shop(self, interaction: Interaction, item: str):
+        shop_pets = {
+            '1': Pet(health=100, stamina=120, defense=5, shop_cost=400),
+            '2': Pet(health=100, stamina=140, defense=10, shop_cost=800),
+            '3': Pet(health=100, stamina=160, defense=15, shop_cost=1200),
+            '4': Pet(health=100, stamina=160, defense=15, shop_cost=1600),
+            '5': Pet(health=100, stamina=160, defense=20, shop_cost=2000)
+        }
+        message_items = '\n'.join([f'{item=}; {shop_pets[item].shop_cost} монет; {shop_pets[item]}' for item in shop_pets])
+
+        if item not in shop_pets or item in ('?', 'help', 'items'):
+            await interaction.response.send_message(f'Доступные для покупки артефакты:\n{message_items}')
             return
 
-        shop_pets = {
-            '1': Pet(health=110, stamina=125, defense=5, shop_cost=400),
-            '2': Pet(health=130, stamina=140, defense=10, shop_cost=800),
-            '3': Pet(health=160, stamina=160, defense=15, shop_cost=1200),
-            '4': Pet(health=225, stamina=200, defense=15, shop_cost=1600),
-            '5': Pet(health=300, stamina=220, defense=20, shop_cost=2000)
-        }
-
         if self.coins < shop_pets[item].shop_cost:
-            await interaction.response.send_message(f'Недостаточно денег: чтобы купить этого пета, нужно {shop_pets[item].shop_cost} денег')
+            await interaction.response.send_message(f'Недостаточно денег: чтобы купить этот артефакт, нужно {shop_pets[item].shop_cost} денег')
             return
 
         self.coins -= shop_pets[item].shop_cost
         self.pet = deepcopy(shop_pets[item])
-        await interaction.response.send_message(f'Ваш новый пет: {self.pet}')
+        await interaction.response.send_message(f'Ваш пет надел артефакт: {self.pet}')
